8d342d521ae51b7baf3d112d7e24422366736755
darshan777 <sanghavi.da@husky.neu.edu>
2018-11-02 16:24:22 -0400

    adding mispell service

---
+++ Validity_Retest/src/main/java/com/validity/demo/service/main/RemoveMispell.java
@@ -0,0 +1,9 @@
+package com.validity.demo.service.main;
+
+import com.validity.demo.helper.CsvData;
+
+import java.util.ArrayList;
+
+public interface RemoveMispell {
+    public ArrayList<CsvData> removeMissSpell();
+}
